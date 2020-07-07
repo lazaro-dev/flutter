fa9213af88297a98aaee0e9b0d5fa634170c0124
lazaro-dev <lazaro.sobral@facape.br>
2020-07-07 18:06:01 -0300

    segundo

---
+++ lib/data/dummy_users.dart
@@ -0,0 +1,25 @@
+import 'package:hello_flutter/models/user.dart';
+
+const DUMMY_USERS = {
+  '1': const User(
+    id: '1',
+    name: 'Cicrano',
+    email: 'Cicrano@outlook.com',
+    avatarUrl:
+        'https://cdn.pixabay.com/photo/2016/11/18/23/38/child-1837375_960_720.png',
+  ),
+  '2': const User(
+    id: '2',
+    name: 'Fulano',
+    email: 'Fulano@outlook.com',
+    avatarUrl:
+        'https://cdn.pixabay.com/photo/2016/03/31/19/58/avatar-1295429_960_720.png',
+  ),
+  '3': const User(
+    id: '3',
+    name: 'Beltrano',
+    email: 'Beltrano@outlook.com',
+    avatarUrl:
+        'https://cdn.pixabay.com/photo/2016/08/28/13/12/secondlife-1625903_960_720.jpg',
+  ),
+};
